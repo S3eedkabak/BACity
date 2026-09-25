7b4693feada0f6ffbf75d052c385728cf00b6dc4
Saeid Kabak <saeedkabak@gmail.com>
2026-09-26 01:09:35 +0200

    feat: register OAuth identity model

--- BACity-/services/api/app/models/__init__.py
+++ BACity-/services/api/app/models/__init__.py
@@ -2,8 +2,9 @@ from app.models.source import Source
 from app.models.venue import Venue
 from app.models.event import Event
 from app.models.user import User
+from app.models.oauth_identity import OAuthIdentity
 from app.models.saved_event import SavedEvent
 from app.models.event_source import EventSource
 from app.models import community
 
-__all__ = ["Source", "Venue", "Event", "User", "SavedEvent"]
+__all__ = ["Source", "Venue", "Event", "User", "OAuthIdentity", "SavedEvent"]
